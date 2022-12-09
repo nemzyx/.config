f8db74b481192c564e33e8b17e70022205ca2db4
ferry-creator <ferryisworking@gmail.com>
2022-12-09 01:52:12 +0100

    init demoscene shit ✨

---
+++ igcc/_on_load.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+// #include <bits/stdc++.h>
+// <bit/stdc++.h> is apparently a lib to
+// import many libs...
+//
+
+using namespace std;
+
+// template <typename T>
+// void print_vector(const vector<T>& xs) {
+//     for (const T& x : xs)
+//         cout << x << endl;
+// }
+
